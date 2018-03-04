76a9b355083b76c39c0670b39e69cdb43c8fdbbc
kollipaka <manikantasai007.msk@gmail.com>
2018-03-04 09:50:49 +0530

    commited

---
+++ data.js
@@ -0,0 +1,33 @@
+function getfile(file,callback){
+   var xhr=new XMLHttpRequest();
+   xhr.overrideMimeType("application/json");
+   xhr.open("GET",file,true);
+   xhr.onreadystatechange=function(){
+     if(xhr.readyState === 4 && xhr.status == "200"){
+       callback(xhr.responseText);
+     }
+   };
+   xhr.send();
+}
+getfile("data.json",function(text){
+  let data=JSON.parse(text);
+  console.log(data);
+  basicinfo(data.basic);
+});
+function basicinfo(basic){
+  var profile=document.getElementById("basics");
+  var name=document.createElement("h2");
+  name.innerHTML=basic.name;
+  profile.appendChild(name);
+  var phone=document.createElement("h3");
+  phone.innerHTML=basic.phone;
+    profile.appendChild(phone);
+    var email=document.createElement("h3");
+    email.innerHTML=basic.email;
+      profile.appendChild(email);
+
+    var photo=document.createElement("h3");
+    photo.innerHTML=basic.photo;
+      profile.appendChild(photo);
+
+}
